5ec06e63dace1932b5d7e5ae5c12be9bbb9ca9a2
chillyme <511105554@qq.com>
2021-06-01 15:53:39 +0800

    [update]优化Datepicker组件

--- src/components/datepicker/src/datepicker.js
+++ src/components/datepicker/src/datepicker.js
@@ -1,243 +1,109 @@
 import Emitter from "vui-design/mixins/emitter";
+import PropTypes from "vui-design/utils/prop-types";
 import DatePicker from "vue2-datepicker";
 import "vue2-datepicker/locale/zh-cn";
 
 const VuiDatepicker = {
-	name: "vui-datepicker",
+  name: "vui-datepicker",
+  components: {
+    DatePicker
+  },
+  mixins: [
+    Emitter
+  ],
+  props: {
+    classNamePrefix: PropTypes.string,
+    range: PropTypes.bool.def(false),
+    format: PropTypes.string.def("YYYY-MM-DD"),
+    valueType: PropTypes.string.def("format"),
+    defaultValue: PropTypes.object,
+    value: {},
+    lang: PropTypes.object,
+    placeholder: PropTypes.string.def(""),
+    editable: PropTypes.bool.def(true),
+    clearable: PropTypes.bool.def(true),
+    confirm: PropTypes.bool.def(false),
+    confirmText: PropTypes.string.def("OK"),
+    multiple: PropTypes.bool.def(false),
+    disabled: PropTypes.bool.def(false),
+    disabledDate: PropTypes.func,
+    disabledTime: PropTypes.func,
+    appendToBody: PropTypes.bool.def(true),
+    inline: PropTypes.bool.def(false),
+    inputClass: PropTypes.string.def("mx-datepicker-input"),
+    inputAttr: PropTypes.object,
+    open: PropTypes.bool,
+    defaultPanel: PropTypes.string,
+    popupStyle: PropTypes.object,
+    popupClass: {},
+    shortcuts: PropTypes.array,
+    titleFormat: PropTypes.string.def("YYYY-MM-DD"),
+    partialUpdate: PropTypes.bool.def(false),
+    rangeSeparator: PropTypes.string.def(" ~ "),
+    showWeekNumber: PropTypes.bool.def(false),
+    hourStep: PropTypes.number.def(1),
+    minuteStep: PropTypes.number.def(1),
+    secondStep: PropTypes.number.def(1),
+    hourOptions: PropTypes.array,
+    minuteOptions: PropTypes.array,
+    secondOptions: PropTypes.array,
+    showHour: PropTypes.bool,
+    showMinute: PropTypes.bool,
+    showSecond: PropTypes.bool,
+    use12h: PropTypes.bool,
+    showTimeHeader: PropTypes.bool.def(false),
+    timeTitleFormat: PropTypes.string.def("YYYY-MM-DD"),
+    timePickerOptions: PropTypes.object,
+    prefixClass: PropTypes.string.def("mx"),
+    scrollDuration: PropTypes.number.def(100),
+    validator: PropTypes.bool.def(true)
+  },
+  data() {
+    const state = {
+      value: this.value
+    };
 
-	components: {
-		DatePicker
-	},
+    return {
+      state
+    };
+  },
+  watch: {
+    value(value) {
+      this.state.value = value;
+    }
+  },
+  methods: {
+    handleInput(value) {
+      this.state.value = value;
+      this.$emit("input", value);
 
-	mixins: [
-		Emitter
-	],
+      if (this.validator) {
+        this.dispatch("vui-form-item", "change", value);
+      }
+    },
+    handleChange(value, type) {
+      this.$emit("change", value, type);
+    }
+  },
+  render() {
+    const { $slots: slots, $props: props, $listeners: listeners } = this;
 
-	props: {
-		type: {
-			type: String,
-			default: "date",
-			validator: value => ["date", "datetime", "year", "month", "time", "week"].indexOf(value) > -1
-		},
-		range: {
-			type: Boolean,
-			default: false
-		},
-		format: {
-			type: String,
-			default: "YYYY-MM-DD"
-		},
-		valueType: {
-			type: String,
-			default: "format"
-		},
-		defaultValue: {
-			type: Date,
-			default: () => new Date()
-		},
-		value: {
+    const attributes = {
+      props: {
+        ...props,
+        value: this.state.value
+      },
+      on: {
+        ...listeners,
+        input: this.handleInput,
+        change: this.handleChange
+      }
+    };
 
-		},
-		lang: {
-			type: Object,
-			default: undefined
-		},
-		placeholder: {
-			type: String,
-			default: ""
-		},
-		editable: {
-			type: Boolean,
-			default: true
-		},
-		clearable: {
-			type: Boolean,
-			default: true
-		},
-		confirm: {
-			type: Boolean,
-			default: false
-		},
-		confirmText: {
-			type: String,
-			default: "OK"
-		},
-		multiple: {
-			type: Boolean,
-			default: false
-		},
-		disabled: {
-			type: Boolean,
-			default: false
-		},
-		disabledDate: {
-			type: Function,
-			default: undefined
-		},
-		disabledTime: {
-			type: Function,
-			default: undefined
-		},
-		appendToBody: {
-			type: Boolean,
-			default: true
-		},
-		inline: {
-			type: Boolean,
-			default: false
-		},
-		inputClass: {
-			type: String,
-			default: "mx-datepicker-input"
-		},
-		inputAttr: {
-			type: Object,
-			default: undefined
-		},
-		open: {
-			type: Boolean,
-			default: undefined
-		},
-		defaultPanel: {
-			type: String,
-			default: undefined
-		},
-		popupStyle: {
-			type: Object,
-			default: undefined
-		},
-		popupClass: {
-			default: undefined
-		},
-		shortcuts: {
-			type: Array,
-			default: undefined
-		},
-		titleFormat: {
-			type: String,
-			default: "YYYY-MM-DD"
-		},
-		partialUpdate: {
-			type: Boolean,
-			default: false
-		},
-		rangeSeparator: {
-			type: String,
-			default: " ~ "
-		},
-		showWeekNumber: {
-			type: Boolean,
-			default: false
-		},
-		hourStep: {
-			type: Number,
-			default: 1
-		},
-		minuteStep: {
-			type: Number,
-			default: 1
-		},
-		secondStep: {
-			type: Number,
-			default: 1
-		},
-		hourOptions: {
-			type: Array,
-			default: undefined
-		},
-		minuteOptions: {
-			type: Array,
-			default: undefined
-		},
-		secondOptions: {
-			type: Array,
-			default: undefined
-		},
-		showHour: {
-			type: Boolean,
-			default: undefined
-		},
-		showMinute: {
-			type: Boolean,
-			default: undefined
-		},
-		showSecond: {
-			type: Boolean,
-			default: undefined
-		},
-		use12h: {
-			type: Boolean,
-			default: undefined
-		},
-		showTimeHeader: {
-			type: Boolean,
-			default: false
-		},
-		timeTitleFormat: {
-			type: String,
-			default: "YYYY-MM-DD"
-		},
-		timePickerOptions: {
-			type: Object,
-			default: null
-		},
-		prefixClass: {
-			type: String,
-			default: "mx"
-		},
-		scrollDuration: {
-			type: Number,
-			default: 100
-		},
-		validator: {
-			type: Boolean,
-			default: true
-		}
-	},
-
-	data() {
-		return {
-			stateValue: this.value
-		};
-	},
-
-	watch: {
-		value(value) {
-			this.stateValue = value;
-		}
-	},
-
-	methods: {
-		handleChange(value) {
-			this.stateValue = value;
-			this.$emit("input", value);
-			this.$emit("change", value);
-
-			if (this.validator) {
-				this.dispatch("vui-form-item", "change", value);
-			}
-		}
-	},
-
-	render() {
-		const { $slots: slots, $props: props, $listeners: listeners } = this;
-
-		const attributes = {
-			props: {
-				...props,
-				value: this.stateValue
-			},
-			on: {
-				...listeners,
-				input: this.handleChange,
-				change: this.handleChange
-			}
-		};
-
-		return (
-			<DatePicker {...attributes} />
-		);
-	}
+    return (
+      <DatePicker {...attributes} />
+    );
+  }
 };
 
 export default VuiDatepicker;
