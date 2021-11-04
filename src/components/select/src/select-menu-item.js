fafc8ba723550f2e6a81b1f7e813ec2ead344db9
vui-design <vui.design@aliyun.com>
2021-11-04 21:56:23 +0800

    [improve]通过鼠标导航Select选项时，禁止将选项滚动到可视区域，避免视觉抖动

--- src/components/select/src/select-menu-item.js
+++ src/components/select/src/select-menu-item.js
@@ -86,7 +86,7 @@ const VuiSelectMenuItem = {
           return;
         }
 
-        if (!vuiSelectState.activedEventType !== "navigate") {
+        if (vuiSelectState.activedEventType !== "navigate") {
           return;
         }
 
--- src/index.js
+++ src/index.js
@@ -374,7 +374,7 @@ export {
 };
 
 export default {
-  version: "1.8.3",
+  version: "1.8.4",
   install,
   // Locale
   locale: locale.use,
